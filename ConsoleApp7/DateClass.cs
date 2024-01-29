c0146090173663ec1d0b4060b067467c7278a0be
stacy2906 <anastasiya.tkachyk.290606@gmail.com>
2024-01-29 09:47:01 +0200

    add program.cs

---
+++ ConsoleApp7/DateClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApp7
+{
+    internal class DateClass
+    {
+        DateTime date;
+
+        public DateClass()
+        {
+        }
+
+        public DateClass(int year, int month, int day)
+        {
+            date = new DateTime(year, month, day);
+        }
+
+        public DateTime Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+        public DateTime PrevDay()
+        {
+            return date.AddDays(-1);
+        }
+
+        public DateTime NextDay()
+        {
+            return date.Add(TimeSpan.FromDays(1));
+        }
+
+        public int DaysLeft()
+        {
+            return DateTime.DaysInMonth(date.Year, date.Month) - date.Day;
+        }
+       
+
+    }
+}
---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateClass dateClass = new DateClass();
+
+            dateClass.PrevDay();
+            dateClass.NextDay();
+            dateClass.DaysLeft();
+
+            Console.ReadKey();
+        }
+    }
+}
